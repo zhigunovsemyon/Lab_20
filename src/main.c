c653f26781f702348d8f4f71c5dc61bd304bbfb3
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-04-04 18:18:15 +0300

    Доделана функция чтения из текстового файла, первая версия вывода матрицы

--- src/main.c
+++ src/main.c
@@ -14,17 +14,16 @@
 	создание из данных в бинарном файле;
 	уничтожение массива.
 */
-/*Текстовый файл содержит в первой строке первым 
+/*Текстовый файл содержит в первой строке первым
  * числом количество строк, в следующих числах этой строки -- размеры строк,
  * дальнейшие строки содержат непосредственно числа*/
+#include <malloc.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <malloc.h>
-#include <stdlib.h>
 
 // Определение типа элементов массива
-#define Element int16_t
-#define EL_SPEC "%hd"
+#define Element long double
+#define EL_SPEC "%Lf"
 
 enum Errors
 {
@@ -34,54 +33,68 @@ enum Errors
 /*Функция считает сумму элементов данного массива arr из элементов Amount */
 int16_t SumOfArray(int16_t *arr, uint16_t Amount)
 {
-	if(!arr)		//Если массива нет -- возврат 0
+	if (!arr) // Если массива нет -- возврат 0
 		return 0;
-	
+
 	/*Перебор элементов массива */
 	int16_t Sum = arr[0];
 	for (int16_t i = 1; i < Amount; ++i)
 		Sum += arr[i];
 
-	return Sum; //Возврат суммы
+	return Sum; // Возврат суммы
+}
+
+void PrintRiggedMatrix(Element **m)
+{
+	for (uint16_t i = 0; i < ((uint16_t*)m)[-1]; i++)
+	{
+		for (uint16_t j = 0; j < ((uint16_t*)(m[i]))[-1]; j++)
+			printf("%Lf\t", m[i][j]);
+		putchar('\n');
+	}
 }
 
 /*Функция осуществляет чтение из текстового файла с названием fname
  * Возвращает NULL в случае ошибки, либо указатель на первый элемент массива*/
 Element **ReadTextFile(const char *fname)
 {
-	FILE *f = fopen("fname", "rt");
+	FILE *f = fopen(fname, "rt");
 	// Если не удалось открыть файл, то возвращается NULL
 	if (!f)
 		return NULL;
 
 	uint16_t lines;
-	//Чтение числа строк, выход с возвратом NULL при неудаче
-	if(!fscanf(f,EL_SPEC, &lines))
+	// Чтение числа строк, выход с возвратом NULL при неудаче
+	if (!fscanf(f, "%hu", &lines))
 		return NULL;
-	//Выделение памяти под массив величин строк, возврат NULL при неудаче
-	uint16_t *cols = (uint16_t*)malloc(sizeof(uint16_t) * lines);
+	// Выделение памяти под массив величин строк, возврат NULL при неудаче
+	uint16_t *cols = (uint16_t *)malloc(sizeof(uint16_t) * lines);
 	if (!cols)
 		return NULL;
 
 	/*Запись размеров каждой строки в массив размеров*/
-	for(uint16_t i = 0; i < lines; ++i)
+	for (uint16_t i = 0; i < lines; ++i)
 	{
-		//Чтение числа строк, выход с возвратом NULL при неудаче
-		if(!fscanf(f, "%hu", cols + i))
+		// Чтение числа строк, выход с возвратом NULL при неудаче
+		if (!fscanf(f, "%hu", cols + i))
 			return NULL;
 	}
-	
-	//Выделение памяти под массив и размеры
-	Element **p = malloc(sizeof(uint16_t) * (1 + lines)
-					+ sizeof(Element) * SumOfArray(cols, lines));
-	p = (uint16_t*)p + 1; //Сдвиг указателя
 
-	((uint16_t*)p)[-1] = lines;
+	// Выделение памяти под массив и размеры
+	int mem = (sizeof(uint16_t) * (1 + lines) + sizeof(Element) * SumOfArray(cols, lines));
+	printf("Allocated memory: %d\n", mem);
+	Element **p = (Element**)malloc(mem);
+	if (!p)
+		return NULL;
+
+	p = (uint16_t *)p + 1; // Сдвиг указателя
+
+	((uint16_t *)p)[-1] = lines;
 	for (int i = 0; i < lines; ++i)
 	{
-		((uint16_t)p)[i] = 
-		p[i][0] = cols[i];
-		p[i] = ((uint16_t*)p[i]) + sizeof(uint16_t);
+		p[i] = p + sizeof(uint16_t *) * lines + i * (sizeof(uint16_t) + sizeof(Element) * cols[i] + sizeof(uint16_t));
+		((uint16_t*)p[i])[0] = cols[i];
+		p[i] = (uint16_t*)(p[i]) + 1;
 		for (uint16_t j = 0; j < cols[i]; j++)
 		{
 			fscanf(f, EL_SPEC, p[i] + j);
@@ -90,10 +103,26 @@ Element **ReadTextFile(const char *fname)
 
 	free(cols);
 	fclose(f);
+	return p;
 }
 
-int main(void)
+int main(int argc, const char **args)
 {
-	ReadTextFile("1.txt");
+	if (argc != 2)
+	{
+		puts("Укажите имя файла!");
+		return NOERR;
+	}
+	Element **m = ReadTextFile(args[1]);
+	if (!m)
+	{
+		puts("Получен NULL!");
+		return NOERR;
+	}
+	PrintRiggedMatrix(m);
+
+	getchar();
+	free(((uint16_t*)m)- 1);
+	while (getchar() == '\n');
 	return NOERR;
 }
